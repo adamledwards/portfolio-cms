d8c2d907b34e2d175b8c509f849d237e47b26b90
Adam Edwards <adamedwards88@gmail.com>
2018-01-19 20:32:48 +0000

    stage host

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,5 +2,5 @@ const path = require('path');
 module.exports = {
   MEDAIA_ROOT: path.resolve(__dirname, '..', '..', 'uploads'),
   MEDAIA_PATH: '/media',
-  HOST: 'http://localhost:4000'
+  HOST: '//ad.aeweb.io/'
 };
